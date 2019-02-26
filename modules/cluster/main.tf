5b4c27af1701faf80a59f56974ac2e168923d658
terrafying <38737064+terrafying@users.noreply.github.com>
2019-02-26 11:23:35 -0500

    Update main.tf

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -88,6 +88,15 @@ resource "aws_security_group" "instance" {
   tags        = "${merge(var.tags, map("Name", format("%s-container-instance", var.name)))}"
 }
 
+resource "aws_security_group_rule" "allow_all_from_10" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 9000
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/0"]
+  security_group_id = "${aws_security_group.instance.id}"
+}
+
 resource "aws_security_group_rule" "instance_out_all" {
   type              = "egress"
   from_port         = 0
